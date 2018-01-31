ea6934e606559e6a678624e7db452566c47036c2
Vibashan VS <34249645+Vibashan@users.noreply.github.com>
2018-02-01 03:50:11 +0530

    Update CNN_flower_classification.py

--- CNN_flower_classification.py
+++ CNN_flower_classification.py
@@ -149,13 +149,5 @@ def optimize(num_iterations):
                       "Train Accuracy = ", session.run(accuracy, feed_dict=feed_dict_train), 
                       "Test Accuracy = ", session.run(accuracy, feed_dict={x:test_x[0:500,:], y_true: test_y[0:500]}), 
                       "Duration = %.1f sec" % duration)
-            #acc = session.run(accuracy, feed_dict=feed_dict_train)
-            #msg = "Optimization Iteration: {0:>6}, Training Accuracy: {1:>6.1%}"
-            #print(msg.format(i + 1, acc))
-
-    total_iterations += num_iterations
-    end_time = time.time()
-    time_dif = end_time - start_time
-    print("Time usage: " + str(timedelta(seconds=int(round(time_dif)))))
 
 optimize(num_iterations=50)                                                        
